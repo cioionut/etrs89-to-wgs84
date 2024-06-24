8acd76dc92135ab8acf4c7acb38006eec9f587d6
Ionut <ionutnciocoiu@gmail.com>
2024-06-24 16:06:46 +0300

    Convert files

--- stereo70_to_etrs89.py
+++ stereo70_to_etrs89.py
@@ -38,6 +38,44 @@ def stereo70_to_etrs89_with_pytransdatro(t: TransRO, north: float, east: float, 
     return math.degrees(lat), math.degrees(lon), h
 
 
+def stereo70_to_etrs89_shape(poly_gdf: gpd.GeoDataFrame, height: float, target_refsys_epsg: int):
+    # convert using PyTransdatRo
+    t = TransRO()
+
+    new_poly_gdf = gpd.GeoDataFrame()
+
+    # extract coords
+    stereo70_coords = extract_coords(poly_gdf)
+
+    # iterate over points
+    new_points = []
+    for stereo70 in stereo70_coords:
+        east, north = stereo70[0], stereo70[1]
+        # convert point
+        lat, lon, alt = stereo70_to_etrs89_with_pytransdatro(
+            t=t,
+            north=north,
+            east=east,
+            height=height)
+        new_points.append([lon, lat])
+
+    new_poly = Polygon(new_points)
+    new_poly_gdf.set_geometry([new_poly], inplace=True,
+                              crs=f"EPSG:{target_refsys_epsg}")
+    return new_poly_gdf
+
+
+def stereo70_to_etrs89_and_save(poly_gdf: gpd.GeoDataFrame, height: float, target_refsys_epsg: int, output_file_type: str, destination_path: str):
+    new_poly_gdf = stereo70_to_etrs89_shape(
+        poly_gdf, height, target_refsys_epsg)
+    # write data to disk
+    if output_file_type == 'geojson':
+        new_poly_gdf.to_file(destination_path,
+                             driver="GeoJSON", drop='crs')
+    else:
+        new_poly_gdf.to_file(destination_path, driver="ESRI Shapefile")
+
+
 def main():
     ############
     # settings #
@@ -47,10 +85,11 @@ def main():
     admin_unit_id = 179169  # Bucuresti, Sector 3
     refsys = 3844
 
-    target_refsys = 9059  # ETRF89
-    target_refsys = 9067  # ETRF2000
-    target_refsys = 9755  # wgs84 latest - https://en.wikipedia.org/wiki/World_Geodetic_System
-    target_refsys = 9000  # ITRF2014
+    target_refsys = 4258  # ETRS89
+    # target_refsys = 9059  # ETRF89
+    # target_refsys = 9067  # ETRF2000
+    # target_refsys = 9755  # wgs84 latest - https://en.wikipedia.org/wiki/World_Geodetic_System
+    # target_refsys = 9000  # ITRF2014
     # target_refsys = 9990  # ITRF2020
 
     # define data directories
@@ -58,6 +97,8 @@ def main():
         'data', 'from_ANCPI', str(county_id), str(admin_unit_id), str(refsys))
     grid_file_path = pathlib.Path(os.path.join(
         'data', 'stereo70_etrs89A.gsb')).resolve()
+    conversion_data_dir = os.path.join(
+        'data', 'convert_transdatro', str(county_id), str(admin_unit_id), str(target_refsys))
 
     # read file
     source_path = os.path.join(ancpi_data_dir, f"{area_id}.json")
@@ -66,12 +107,26 @@ def main():
 
     # convert from Stereo70  to ETRS89, more precise
     # convert from EPSG:3844 to EPSG:9067 (etrf2000)
+    # convert from EPSG:3844 to EPSG:4258 (?)
 
-    # extract coords
-    stereo70_coords = extract_coords(poly_gdf)
+    height = 64  # assume height
+    conversion_data_dir = os.path.join(conversion_data_dir, f"{area_id}")
+    ensure_path_exists(conversion_data_dir)
+    destination_path = os.path.join(
+        conversion_data_dir, f"{area_id}.shp")
+    stereo70_to_etrs89_and_save(
+        poly_gdf=poly_gdf,
+        height=height,
+        target_refsys_epsg=target_refsys,
+        output_file_type='shp',
+        destination_path=destination_path
+    )
+    return
 
     # iterate over points
     height = 64  # assume height
+    # extract coords
+    stereo70_coords = extract_coords(poly_gdf)
     for stereo70 in stereo70_coords:
         east, north = stereo70[0], stereo70[1]
         break
@@ -84,7 +139,8 @@ def main():
     )
     alt = 112
     if height == alt:
-        raise ValueError("Please adjust manually the altitude, the conversion only works in 2d")
+        raise ValueError(
+            "Please adjust manually the altitude, the conversion only works in 2d")
     print(lat, lon, alt)
 
     # convert using PyTransdatRo
